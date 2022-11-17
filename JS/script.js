09542fa4761d683289a4243a556f672b4d34b857
Kumard <kumard@rbos.co.uk>
2022-11-17 18:00:16 +0530

    login form error removed

--- JS/script.js
+++ JS/script.js
@@ -1,93 +1,113 @@
-//form validations:
-
-function validation() {
-
-  let pass1 = document.getElementById("cexampleInputPassword1");
-  let submitemail = document.getElementById("exampleInputEmail1");
-  let mobileNumber = document.getElementById("exampleInputMobile")
-  let userFName = document.getElementById("exampleInputFName");
-  let userLName = document.getElementById("exampleInputLName");
-  let accountNumber = document.getElementById("accountNumber");
-  let useraddress = document.getElementById("exampleInputAddress");
-
-
-
-  //for name
-  let userFName2 = userFName.value.trim();
-  if (!/^[a-z A-Z \.]{3,}$/.test(userFName2)) {
-    alert("Please enter valid first name");
-    userFName.focus();
-    return false;
+document.getElementById('uname').addEventListener('keyup', function () {
+  var uname = document.getElementById('uname').value
+  var exp1 = /^[a-z A-Z \.]{3,}$/
+  if (exp1.test(uname.trim())) {
+    document.getElementById('uname').className = 'valid form-control'
+    document.getElementById('uname').style.border = '2px solid green'
+    document.getElementById('para1').style.visibility = 'hidden'
+  } else {
+    document.getElementById('uname').className = 'Invalid form-control'
+    document.getElementById('para1').style.visibility = 'visible'
+    document.getElementById('uname').style.border = '2px solid red'
   }
+})
 
-  //for last name
-  let userLName2 = userLName.value.trim();
-  if (!/^[a-z A-Z \.]{3,}$/.test(userLName2)) {
-    alert("Please enter valid last name");
-    userLName.focus();
-    return false;
+document.getElementById('accnumber').addEventListener('keyup', function () {
+  var accnumber = document.getElementById('accnumber').value
+  var exp2 = /^[0-9]{8}$/
+  if (exp2.test(accnumber)) {
+    document.getElementById('accnumber').className = 'valid form-control'
+    document.getElementById('accnumber').style.border = '2px solid green'
+    document.getElementById('para2').style.visibility = 'hidden'
+  } else {
+    document.getElementById('accnumber').className = 'Invalid form-control'
+    document.getElementById('para2').style.visibility = 'visible'
+    document.getElementById('accnumber').style.border = '2px solid red'
   }
+})
 
-  //for account number
-  if (!/^[0-9]{8}$/.test(accountNumber.value)) {
-    alert("Please enter 8 digits of your Account Number");
-    accountNumber.focus();
-    return false;
+document.getElementById('address').addEventListener('keyup', function () {
+  var address = document.getElementById('address').value
+  var exp3 = /^[a-z A-Z 0-9\.]{10,}$/
+  if (exp3.test(address.trim())) {
+    document.getElementById('address').className = 'valid form-control'
+    document.getElementById('address').style.border = '2px solid green'
+    document.getElementById('para3').style.visibility = 'hidden'
+  } else {
+    document.getElementById('address').className = 'Invalid form-control'
+    document.getElementById('para3').style.visibility = 'visible'
+    document.getElementById('address').style.border = '2px solid red'
   }
+})
 
-  //for address
-  let useraddress2 = useraddress.value.trim();
-  if (!/^[a-z A-Z 0-9 .,\.]{10,}$/.test(useraddress2)) {
-    alert("Please enter valid address with atleast 20 characters, symbols not allowed");
-    useraddress.focus();
-    return false;
-  }
+document.getElementById('email').addEventListener('keyup', function () {
+    var email = document.getElementById('email').value
+    var exp4 = /^([a-zA-Z\.-]+)@([a-zA-Z-]+)\.([a-z]{2,8})(.[a-z]{2,8})?$/
+    if (exp4.test(email.trim())) {
+      document.getElementById('email').className = 'valid form-control'
+      document.getElementById('email').style.border = '2px solid green'
+      document.getElementById('para4').style.visibility = 'hidden'
+    } else {
+      document.getElementById('email').className = 'Invalid form-control'
+      document.getElementById('para4').style.visibility = 'visible'
+      document.getElementById('email').style.border = '2px solid red'
+    }
+  })
 
-  //for email 
+  document.getElementById('mobile').addEventListener('keyup', function () {
+    var mobile = document.getElementById('mobile').value
+    var exp5 = /^[6-9][0-9]{9}$/
+    if (exp5.test(mobile)) {
+      document.getElementById('mobile').className = 'valid form-control'
+      document.getElementById('mobile').style.border = '2px solid green'
+      document.getElementById('para5').style.visibility = 'hidden'
+    } else {
+      document.getElementById('mobile').className = 'Invalid form-control'
+      document.getElementById('para5').style.visibility = 'visible'
+      document.getElementById('mobile').style.border = '2px solid red'
+    }
+  })
 
-  if (!/^([a-zA-Z 0-9\.-]+)@([a-zA-Z-]+)\.([a-z]{2,8})(.[a-z]{2,8})?$/.test(submitemail.value)) {
-    alert("Please enter valid email address");
-    submitemail.focus();
-    return false;
-  }
+  document.getElementById('newPassword').addEventListener('keyup', function () {
+    var newPassword = document.getElementById('newPassword').value
+    var exp6 = /^(?=.*\d)(?=.*[a-z])(?=.*[A-Z]).{6,20}$/
+    if (exp6.test(newPassword)) {
+      document.getElementById('newPassword').className = 'valid form-control'
+      document.getElementById('newPassword').style.border = '2px solid green'
+      document.getElementById('errorpassword').style.visibility = 'hidden'
+    } else {
+      document.getElementById('newPassword').className = 'Invalid form-control'
+      document.getElementById('errorpassword').style.visibility = 'visible'
+      document.getElementById('newPassword').style.border = '2px solid red'
+    }
+  })
 
-//for mobile
-  if (!/^[6-9][0-9]{9}$/.test(mobileNumber.value)) {
-    alert("Only numbers are allowed for mobile number field starting with 6 to 9");
-    mobileNumber.focus();
-    return false;
-  }
 
-  //To check a password must be between 6 to 20 characters which contain at least one numeric digit, one uppercase and one lowercase letter
-  var passw = /^(?=.*\d)(?=.*[a-z])(?=.*[A-Z]).{6,20}$/;
-  if (pass1.value.match(passw)) {
-    return true;
-  }
-  else {
-    alert('password must be between 6 to 20 characters which contain at least one numeric digit, one uppercase and one lowercase letter.')
-    return false;
-  }
+  function formSubmit(input){
+    for (index = 0; index < 4; index++) {
+        if (document.querySelectorAll('input')[index].getAttribute("class") == 'Invalid' || document.querySelectorAll('input')[index].value == "") {
+            alert("Registration not successful")
+            return false
+        }
+    }
+    return true
 }
 
-//form validations end here
-////////////////////////////////////////////////////////////////////////////////////////////////
 
-
-//sign up page data post to db.json server starts here
-////////////////////////////
+/////////////////////////////////////////////////////////////////////
 
 let form = document.getElementById('form')
 form.addEventListener('submit', function (e) {
   //auto submission of form
 
   e.preventDefault()
-  let fname = document.getElementById('exampleInputFName').value
-  let lname = document.getElementById('exampleInputLName').value
-  let accnumber = document.getElementById('accountNumber').value
-  let address = document.getElementById('exampleInputAddress').value
-  let email = document.getElementById('exampleInputEmail1').value
-  let mobile = document.getElementById('exampleInputMobile').value
-  let passw = document.getElementById('cexampleInputPassword1').value
+  let uname = document.getElementById('uname').value
+  // let lname = document.getElementById('exampleInputLName').value
+  let accnumber = document.getElementById('accnumber').value
+  let address = document.getElementById('address').value
+  let email = document.getElementById('email').value
+  let mobile = document.getElementById('mobile').value
+  let newPassword = document.getElementById('newPassword').value
 
 
   //fetch post request
@@ -95,13 +115,12 @@ form.addEventListener('submit', function (e) {
   fetch("http://localhost:3000/CustomerData", {
     method: 'POST',
     body: JSON.stringify({
-      fname: fname,
-      lname: lname,
+      uname: uname,
       address: address,
       accnumber: accnumber,
       email: email,
       mobile: mobile,
-      passw: passw
+      newPassword: newPassword
 
     }),
     headers: {
@@ -113,7 +132,7 @@ form.addEventListener('submit', function (e) {
     })
     .then(function (data) {
       console.log(data)
-      var results = document.getElementById('results')
+      // var results = document.getElementById('results')
       // results.innerHTML = `<p> The title of the to do is ${data.title}</P>
       // <p>The body of the to do is ${data.body}</p>`
       alert("Details have been submitted successfully");
@@ -134,6 +153,15 @@ btnLogin.onclick = login;
 // var currentUser = 0;
 
 function login() {
+
+
+let Lform = document.getElementById('loginFormId')
+Lform.addEventListener('submit', function (e) {
+  //auto submission of form
+
+  e.preventDefault()
+  
+
   console.log("login");
   let email = document.getElementById("exampleInputEmail33").value;
   let password = document.getElementById("exampleInputPassword3").value;
@@ -142,7 +170,7 @@ function login() {
   }
   else if (password === "") {
     alert("password missing");
-
+    
   }
   else {
 
@@ -153,17 +181,19 @@ function login() {
         if (data.length === 0) {
           alert("Email address does not exist")
         }
-        if (data[0].passw === password) {
+        if (data[0].newPassword === password) {
           console.log("Success");
           alert("login successful");
-          return true;
+          window.location = "services.html"
+          // return true;
         }
         else {
           alert("Incorrect Password")
-          return false;
+          // return false;
         }
       })
   }
+})
 }
 /////////////////////////////
 //login script validation ends
